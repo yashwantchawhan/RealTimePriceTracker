2063ca1465dd0e49ab823fbc0697c8e2e97ebf48
Yashwant Chawhan <ynchawhan4@gmail.com>
2025-12-02 08:00:28 +0530

    Adding app class

--- app/src/main/java/com/example/realtimepricetracker/MainActivity.kt
+++ app/src/main/java/com/example/realtimepricetracker/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.realtimepricetracker.ui.theme.RealTimePriceTrackerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/realtimepricetracker/RealPriceTrackerApp.kt
@@ -0,0 +1,7 @@
+package com.example.realtimepricetracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RealPriceTrackerApp: Application() { }
